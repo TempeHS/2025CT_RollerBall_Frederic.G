127b11cc531df12c640fbfb2408e34893a96224f
fredg78 <frederic.garran@education.nsw.gov.au>
2025-03-03 10:35:20 +1100

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+     private Rigidbody rb; 
+     private float movementX;
+     private float movementY;
+     public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
-      
+      rb = GetComponent<Rigidbody>();
     }
 
    void Onmove(InputValue movementValue)
    {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.y;
+        movementY = movementVector.x;
+   }
+   private void FixedUpdate()
+   {
+     Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+     rb.AddForce(movement*speed);
    }
 }
