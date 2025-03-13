fcbc11b0aaee7d3e9c27100396afc32d1707b868
fredg78 <frederic.garran@education.nsw.gov.au>
2025-03-13 15:05:13 +1100

    count

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
      private Rigidbody rb; 
+     private int count;
      private float movementX;
      private float movementY;
      public float speed = 0;
+     public TextMeshProUGUI countText;
     // Start is called before the first frame update
     void Start()
     {
       rb = GetComponent<Rigidbody>();
+      count = 0;
+      SetCountText();
     }
 
    void OnMove(InputValue movementValue)
@@ -23,9 +28,26 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
    }
+   
+   
+   void SetCountText()
+   {
+    countText.text =  "Count: " + count.ToString();
+   }
    private void FixedUpdate()
    {
      Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
      rb.AddForce(movement*speed);
    }
+   private void OnTriggerEnter(Collider other)
+   {
+    if(other.gameObject.CompareTag("Pickup"))
+    {
+      other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
+    }
+    
+   }
+
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rotator : MonoBehaviour
 
    
 {
+    public float speed = 0;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime * speed);
 }
 }
